031df03ee494214e393b3fe2b32071c700b37886
Ruslan <44776391+hararudoka@users.noreply.github.com>
2022-09-15 15:21:02 +0300

    add some comments in config

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,18 +6,24 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+// Some configs for program. TODO: rewrite.
 type Config struct {
+	// Resolution of a screen data.
 	ScreenWidth  int `yaml:"screen_width"`
 	ScreenHeight int `yaml:"screen_height"`
 
+	// Logic things...
 	ToKeepAlive   []int `yaml:"to_keep_alive"`
 	ToBecomeAlive []int `yaml:"to_become_alive"`
 
+	// Size of map, diameter
 	MapSize int `yaml:"map_size"`
 
+	// Tick per second
 	TPS int `yaml:"TPS"`
 }
 
+// Loads config
 func LoadConfig(path string) (config Config, err error) {
 	b, err := ioutil.ReadFile(path)
 	if err != nil {
